4125285e8cd71cef745e3c4ca2aac3311d350821
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-02-05 18:23:07 +0000

    added 02_seeds.sql

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO users (
+    name, email, password) 
+    VALUES ('SLEEF DAVID', '1@a.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (
+  name, email, password) 
+  VALUES ('Arnold Washintonsly', '2@b.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (
+  name, email, password) 
+    VALUES ('G Johnson', '3@c.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+
+INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
+    VALUES ('TITLE', 'DESCRIPTION', 103, 'cat.com', 'cat.net', 2438, 8, 2, 1, true, 'place1', 'place2', 'place3', '9999 place4', '38222');
+
+INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
+    VALUES ('TITLE', 'DESCRIPTION', 103, 'cat.com', 'cat.net', 2438, 8, 2, 1, true, 'place5', 'place6', 'place7', '9999 place8', '38223');
+
+INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
+    VALUES ('TITLE', 'DESCRIPTION', 103, 'cat.com', 'cat.net', 2438, 8, 2, 1, true, 'place9', 'place10', 'place11', '9999 place12', '38224');
+
+
